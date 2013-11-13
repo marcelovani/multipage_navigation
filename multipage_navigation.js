3b70c1e47fc4eba0fb063885de8e743e4bc34cb1
Marcelo Vani <marcellovani@yahoo.co.uk>
2013-11-13 11:41:41 +0000

    #2134267 Fixed the logic that hides dropdown

--- multipage_navigation.js
+++ multipage_navigation.js
@@ -3,15 +3,16 @@
         attach: function (context, settings) {
             $(".block-multipage-navigation .pagination").click(function() {
                 var timesClicked = 0;
-                $(this).find("nav").toggle();
-                $(this).find("span.arrow").toggleClass("upsidedown");
+                element = $(this);
+                element.find("nav").toggle();
+                element.find("span.arrow").toggleClass("upsidedown");
 
                 // Close the navigation when click outside.
                 var handler = function() {
                     timesClicked++;
                     if (timesClicked > 1) {
-                        $(this).find("nav").hide();
-                        $(this).find("span.arrow").removeClass("upsidedown");
+                        element.find("nav").hide();
+                        element.find("span.arrow").removeClass("upsidedown");
                         $("body").unbind("click.navigation");
                     }
                 };
